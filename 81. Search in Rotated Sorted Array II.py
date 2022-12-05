db91f92918d2e4764189da05be0bff6a1e900bae
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2022-12-05 15:51:20 +0900

    first commit

--- 81. Search in Rotated Sorted Array II.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def search(self, nums, target):
-        if target not in nums:
-            return False       
-        
-        for i in nums:
-            if i == target:
-                return True
