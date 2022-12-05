78e58c5a689a72719b2f36d6ba565db7347ed139
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2022-12-05 17:01:03 +0900

    Modify it to O(n) time complexity

--- 1. Two Sum .py
+++ 1. Two Sum .py
@@ -1,7 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+from typing import List
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        from_num_to_index = dict()
         for i in range(len(nums)):
-          for j in range(len(nums)):
-              if i != j and nums[i] + nums[j] == target:
-                  return [i, j]
+            if nums[i] in from_num_to_index:
+                return [from_num_to_index[nums[i]], i]
+            
+            from_num_to_index[target-nums[i]] = i
