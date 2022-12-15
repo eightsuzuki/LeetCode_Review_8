a85f12f0964e4cca16117781ca21ae8717a8b54d
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2022-12-15 23:59:39 +0900

    Add 2089_Find_Target_Indices_After_Sorting_Array

---
+++ 2089_Find_Target_Indices_After_Sorting_Array.py
@@ -0,0 +1,21 @@
+# n = len(nums)
+# time: O(n)
+# space: O(n)
+
+from typing import List
+
+
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+
+        nums.sort()
+        Out = []
+        count = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                Out.append(i)
+                count += 1
+
+        return Out
