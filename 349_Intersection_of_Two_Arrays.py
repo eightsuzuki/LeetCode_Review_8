5d9d7bda0bf2d4595239e3bc8e4ef0125422a20a
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2022-12-21 04:49:42 +0900

    Update 349_Intersection_of_Two_Arrays.py

--- 349_Intersection_of_Two_Arrays.py
+++ 349_Intersection_of_Two_Arrays.py
@@ -9,7 +9,7 @@ from typing import List
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-        out = []
+        intersection_nums = []
 
         set1 = set(nums1)
         set2 = set(nums2)
