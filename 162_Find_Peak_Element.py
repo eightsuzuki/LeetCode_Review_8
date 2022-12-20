691502d3ad80a4a33388d6a74e6edc5fb940bdd8
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2022-12-21 03:34:16 +0900

    Update 162_Find_Peak_Element.py

--- 162_Find_Peak_Element.py
+++ 162_Find_Peak_Element.py
@@ -1,6 +1,6 @@
 # n = len(nums)
-# time: O(n)
-# space: O(n)
+# time: O(log n)
+# space: O(1)
 
 from typing import List
 
@@ -12,12 +12,18 @@ class Solution:
         if len(nums) == 1:
             return 0
 
-        if nums[0] > nums[1]:
-            return 0
-
-        if nums[len(nums)-2] < nums[len(nums)-1]:
-            return len(nums)-1
-
-        for i in range(1, len(nums)-1):
-            if nums[i-1] < nums[i] and nums[i] > nums[i+1]:
-                return i
+        left = 0
+        right = len(nums) - 1
+        
+        while right > left:
+            mid = (left + right) // 2
+            
+            if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]:
+                return mid
+            elif nums[mid] < nums[mid+1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        if nums[right] > nums[mid] :
+            return  right
