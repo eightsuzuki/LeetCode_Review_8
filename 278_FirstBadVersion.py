35e0f134af42fb06869fb95a18cf6bdfa6d2b762
eightsuzuki <suzukieight8@gmail.com>
2023-06-12 02:16:04 +0900

    Add 278

---
+++ 278_FirstBadVersion.py
@@ -0,0 +1,23 @@
+# time: O(logn)
+# space; O(1)
+
+from typing import List
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        
+        left = 1
+        right = n        
+
+        while left < right:
+            mid = (left + right) // 2
+                        
+            if isBadVersion(mid) == False:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left            
