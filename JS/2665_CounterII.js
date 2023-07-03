0ac989d78e85811ababef21fc1baa8a25b2b42db
eightsuzuki <suzukieight8@gmail.com>
2023-07-04 03:07:31 +0900

    add 12 js problems

---
+++ JS/2665_CounterII.js
@@ -0,0 +1,36 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+class Counter {
+    constructor(init) {
+      this.init = init;
+      this.presentCount = init;
+    }
+  
+    increment() {
+      this.presentCount += 1;
+      return this.presentCount;
+    }
+  
+    decrement() {
+      this.presentCount -= 1;
+      return this.presentCount;
+    }
+  
+    reset() {
+      this.presentCount = this.init;
+      return this.presentCount;
+    }
+  }
+  
+  var createCounter = function(init) {
+    return new Counter(init);
+  };
+  
+  /**
+   * const counter = createCounter(5)
+   * counter.increment(); // 6
+   * counter.reset(); // 5
+   * counter.decrement(); // 4
+   */
