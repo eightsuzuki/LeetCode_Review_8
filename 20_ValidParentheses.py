e5028ac73654547591e7447e85efd2475b007b8c
eightsuzuki <suzukieight8@gmail.com>
2023-07-14 21:39:46 +0900

    Add 20

---
+++ 20_ValidParentheses.py
@@ -0,0 +1,24 @@
+# n = len(s)
+# time: O(n)
+# space: O(n)
+
+from typing import List
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if char == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif char == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif char == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return not stack
