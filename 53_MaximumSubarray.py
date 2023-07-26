e519b950735462de7d400dad65e359fee78f4331
eightsuzuki <suzukieight8@gmail.com>
2023-07-27 00:52:39 +0900

    Add 54

--- 53_MaximumSubarray.py
+++ 53_MaximumSubarray.py
@@ -6,15 +6,15 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxi = float("-inf")
+        ans = float("-inf")
         sum = 0
         for i in range(len(nums)):
             sum += nums[i]
 
-            if sum > maxi:
-                maxi = sum
+            if sum > ans:
+                ans = sum
             
             if sum < 0:
                 sum = 0
 
-        return maxi
+        return ans
