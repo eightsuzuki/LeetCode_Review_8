afeeb81351a53fee4d1a055e59b4c3e41556a4a4
eightsuzuki <suzukieight8@gmail.com>
2023-08-13 01:42:55 +0900

    Add 70

--- 70_ClimbingStairs.py
+++ 70_ClimbingStairs.py
@@ -1,7 +1,15 @@
+# time: O(n)
+# space: O(1)
+
+from typing import List
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 3: return n
+
         a = (1, 2)
+
         for i in range(3, n+1):
             a = (a[1], a[0]+a[1])
+
         return a[-1]
