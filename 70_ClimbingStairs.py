7bd7718310139e3a8813a82c1f27dd3517e33843
eightsuzuki <suzukieight8@gmail.com>
2023-08-12 23:55:58 +0900

    Add 70

---
+++ 70_ClimbingStairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: return n
+        a = (1, 2)
+        for i in range(3, n+1):
+            a = (a[1], a[0]+a[1])
+        return a[-1]
