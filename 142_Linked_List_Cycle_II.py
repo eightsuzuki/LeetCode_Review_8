73a0ee45c3bed8bf44c840e98c3ceec2fdf4137f
eightsuzuki <suzukieight8@gmail.com>
2024-01-15 01:19:55 +0900

    Add 141 & 142

---
+++ 142_Linked_List_Cycle_II.py
@@ -0,0 +1,25 @@
+# n is the number of nodes
+# time: O(n)
+# space: O(n)
+
+from typing import List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        visited_nodes = set()
+        current_node = head
+
+        while current_node:
+            if current_node in visited_nodes:
+                return current_node
+
+            visited_nodes.add(current_node)
+            current_node = current_node.next
+            
+        return None
