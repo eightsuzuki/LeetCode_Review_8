b084faa18ddb3d2b163a06a7ab32312d581dfed4
eightsuzuki <suzukieight8@gmail.com>
2024-01-25 22:10:24 +0900

    Add 241

---
+++ 241_Different_Ways_to_Add_Parentheses.py
@@ -0,0 +1,24 @@
+# n = len(expression)
+# time: O(2^n)
+# space: O(n)
+
+from typing import List
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        if input.isdigit():
+            return [int(input)]
+        res = []
+        for i in range(len(input)):
+            if input[i] in "-+*":
+                left = self.diffWaysToCompute(input[:i])
+                right = self.diffWaysToCompute(input[i+1:])
+                for l in left:
+                    for r in right:
+                        if input[i] == '+':
+                            res.append(l+r)
+                        elif input[i] == '-':
+                            res.append(l-r)
+                        elif input[i] == '*':
+                            res.append(l*r)
+        return res
