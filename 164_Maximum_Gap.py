e8cc9152663533aab9ed4d3567cdd5fa2b5fd03a
eightsuzuki <suzukieight8@gmail.com>
2024-02-22 21:23:29 +0900

    Add 164

---
+++ 164_Maximum_Gap.py
@@ -0,0 +1,18 @@
+# n = len(nums)
+# time: O(n)
+# space: O(1)
+
+from typing import List
+
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        min = float("-inf")
+        if len(nums) < 2:
+            return 0
+        for i in range(len(nums) - 1):
+            x = abs(nums[i] - nums[i + 1])
+            if min < x:
+                min = x
+        return min
