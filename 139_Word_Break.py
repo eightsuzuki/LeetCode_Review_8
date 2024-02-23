c80279a41786ac97a47389577911ad11b831c4dc
eightsuzuki <suzukieight8@gmail.com>
2024-02-24 02:26:31 +0900

    Add 139

---
+++ 139_Word_Break.py
@@ -0,0 +1,21 @@
+
+# n = len(s), m = len(wordDict)
+# time: O(n^2 * m)
+# space: O(n)
+
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        max_len = max(map(len, wordDict)) 
+
+        for i in range(1, n + 1):
+            for j in range(i - 1, max(i - max_len - 1, -1), -1): 
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[n]
